3409c7c8ee02b1e5dd50f7d52b3b110d6a3e3ab8
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2025-08-01 20:36:15 +0530

    Define sample function

--- sample.py
+++ sample.py
@@ -1,23 +1,21 @@
-import time
 import tensorflow as tf
 from utils import misc
 
-project_root = misc.get_project_root()
-configs = misc.load_config("config.yaml")
-EXPORT_DIR_NAME = configs["export_dir_name"]
-export_dir = project_root.joinpath(EXPORT_DIR_NAME)
-reloaded_model = tf.saved_model.load(export_dir)
+def sample(initial_input_str: str, sample_size: int, temperature: float = 1.0):
+    configs = misc.load_config("config.yaml")
+    EXPORT_DIR_NAME = configs["export_dir_name"]
+    project_root = misc.get_project_root()
+    saved_model_path = project_root.joinpath(EXPORT_DIR_NAME)
+    reloaded_model = tf.saved_model.load(saved_model_path)
+    states = None
+    next_char = tf.constant([initial_input_str])
+    result = [next_char]
+    for n in range(sample_size):
+        next_char, states = reloaded_model.generate_one_step(inputs=next_char, temperature=temperature, states=states)
+        result.append(next_char)
+    result = tf.strings.join(result)
+    return result
 
-start = time.time()
-states = None
-next_char = tf.constant(['ජාතික'])
-result = [next_char]
-
-for n in range(1000):
-    next_char, states = reloaded_model.generate_one_step(next_char, states=states, temperature=0.75)
-    result.append(next_char)
-
-result = tf.strings.join(result)
-end = time.time()
-print(result[0].numpy().decode('UTF-8'), '\n\n' + '_'*80)
-print('\nRun time:', end - start)
+if __name__ == "__main__":
+    generated_sample = sample(initial_input_str='k', sample_size=100, temperature=0.8)
+    print(generated_sample[0].numpy().decode('UTF-8'))
