b777d8b535a2b9d55ec15a0a880fd75d85768fac
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2025-08-02 18:34:06 +0530

    Refactor training script to separate data cleaning and use config namespaces

--- train.py
+++ train.py
@@ -4,63 +4,41 @@ from datasets import load_dataset
 from utils import misc, model_utils
 from models.gru import GRUModel
 from models.one_step_model import OneStep
+from utils.cleaning import Cleaner
+from types import SimpleNamespace
+from pathlib import Path
 
-def train_char_lm(configs: dict):
-    HF_DATASET = configs["huggingface_dataset"]
-    SEQ_LENGTH = configs["seq_length"]
-    BUFFER_SIZE = configs["buffer_size"]
-    SEED = configs["seed"]
-    BATCH_SIZE = configs["batch_size"]
-    EMBEDDING_DIM = configs["embedding_dim"]
-    RNN_UNITS = configs["rnn_units"]
-    EPOCHS = configs["epochs"]
-    LEARNING_RATE = configs["learning_rate"]
-    TRAIN_FRAC = configs["train_frac"]
-    EXPORT_DIR_NAME = configs["export_dir_name"]
-    DATASET_FRAC = configs["dataset_frac"]
-
+def train_char_lm(configs: dict, text_file_path: Path):
+    # Load configuration parameters
+    configs = SimpleNamespace(**configs)
+    es_configs = SimpleNamespace(**configs.early_stopping)
+                    
+    # Set distribute strategy for training
     strategy = misc.get_distribute_strategy()
 
-    project_root = misc.get_project_root()
-    cache_dir = project_root.joinpath("dataset", "cache")
-    cache_dir.mkdir(parents=True, exist_ok=True)
-    dataset = load_dataset(HF_DATASET, cache_dir=cache_dir)
+    # Load cleaned text data to memory
+    with open(text_file_path, "r") as file:
+        text_data = file.read()
 
-    raw_ds = dataset["train"]
-    content = raw_ds["content"]
-    text_list = model_utils.create_1d_list(content)
-    text = " ".join(text_list)
-
-    if 0 < DATASET_FRAC < 1.0:
-        limit = int(DATASET_FRAC * len(text))
-        text = text[:limit]
-    elif DATASET_FRAC <= 0 or DATASET_FRAC > 1.0:
-        raise ValueError("dataset_frac must be a value between 0 and 1 (0, 1], includes 1.0")
-    
-    cleaned_text = model_utils.clean_text(text)
-
-    processed_dir = project_root.joinpath("dataset", "processed")
-    processed_dir.mkdir(parents=True, exist_ok=True)
-    processed_file = processed_dir.joinpath("cleaned.txt")
-    with open(processed_file, 'w') as file:
-        file.write(cleaned_text)
-    print(f"Saved cleaned text to {processed_file}")
-
+    # Convert the strings to numerical representation
     print("Tokenizing text in progress...")
-    input_ids, vocabulary = model_utils.get_input_ids(cleaned_text)
-
+    input_ids, vocabulary = model_utils.get_input_ids(text_data)
     vocab_size = len(vocabulary)
     print("Tokenization finished")
 
+    # Split the vectorized text into train and validation sets
     ids_dataset = tf.data.Dataset.from_tensor_slices(input_ids)
-    sequences = ids_dataset.batch(SEQ_LENGTH+1, drop_remainder=True)
+    seq_length = configs.seq_length + 1
+    sequences = ids_dataset.batch(seq_length, drop_remainder=True)
     full_dataset = sequences.map(model_utils.split_input_target)
-    shuffled_dataset = full_dataset.shuffle(buffer_size=BUFFER_SIZE, reshuffle_each_iteration=False, seed=SEED)
-    train_set, val_set = model_utils.train_val_split(dataset=shuffled_dataset, train_frac=TRAIN_FRAC)
-    train_set = train_set.batch(BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
-    val_set = val_set.batch(BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
+    shuffled_dataset = full_dataset.shuffle(buffer_size=configs.buffer_size, reshuffle_each_iteration=False, seed=configs.seed)
+    train_set, val_set = model_utils.train_val_split(dataset=shuffled_dataset, train_frac=configs.train_frac)
+    train_set = train_set.batch(configs.batch_size, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
+    val_set = val_set.batch(configs.batch_size, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
 
-    checkpoints_dir = project_root.joinpath("training_checkpoints")
+    # Define checkpoint and early stopping callbacks
+    project_root = misc.get_project_root()
+    checkpoints_dir = project_root.joinpath(configs.checkpoints_dir_name)
     checkpoints_dir.mkdir(parents=True, exist_ok=True)
     checkpoint_prefix = os.path.join(str(checkpoints_dir), "ckpt_{epoch}.weights.h5")
 
@@ -69,41 +47,51 @@ def train_char_lm(configs: dict):
         save_weights_only=True)
 
     early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        min_delta=0.001,
-        patience=2,
-        verbose=1,
-        restore_best_weights=True
+        monitor=es_configs.monitor,
+        min_delta=es_configs.min_delta,
+        patience=es_configs.patience,
+        verbose=es_configs.verbose,
+        restore_best_weights=es_configs.restore_best_weights
     )
 
+    # Create and compile the model within the distribution strategy scope
     with strategy.scope():
-        model = GRUModel(vocab_size=vocab_size, embedding_dim=EMBEDDING_DIM, rnn_units=RNN_UNITS)
-        adam = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+        model = GRUModel(vocab_size=vocab_size, embedding_dim=configs.embedding_dim, rnn_units=configs.rnn_units)
+        adam = tf.keras.optimizers.Adam(learning_rate=configs.learning_rate)
         loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
         model.compile(optimizer=adam, loss=loss)
 
-    model.fit(train_set, validation_data=val_set, epochs=EPOCHS, 
-              callbacks=[checkpoint_callback, early_stopping])
+    # Train the model
+    history = model.fit(train_set, validation_data=val_set, epochs=configs.epochs, callbacks=[checkpoint_callback, early_stopping])
+    
+    # Define a model that makes a single step prediction
+    one_step_model = OneStep(model=model, vocabulary=vocabulary)
 
-    ids_from_chars = tf.keras.layers.StringLookup(vocabulary=vocabulary, mask_token=None)
-    chars_from_ids = tf.keras.layers.StringLookup(vocabulary=vocabulary, mask_token=None, invert=True)
-    one_step_model = OneStep(model=model, chars_from_ids=chars_from_ids, ids_from_chars=ids_from_chars)
-
-    # Call one step model at least twice(n=2) with actual input data so that generate_one_step function will be 
-    # traced (unless the function will not be saved)
+    # Call one step model at least twice(n=2) with dummy data so that generate_one_step function will be traced
     states = None
     temp = tf.constant(0.75)
     next_char = tf.constant(['a'])
     for n in range(100):
         next_char, states = one_step_model.generate_one_step(inputs=next_char, temperature=temp, states=states)
 
-    export_dir = project_root.joinpath(EXPORT_DIR_NAME)
-    try:
-        tf.saved_model.save(one_step_model, export_dir)
-        print(f"Generator model saved to {export_dir}")
-    except Exception as e:
-        print(e)
+    # Export the generator
+    export_dir = project_root.joinpath(configs.export_dir_name)
+    message = model_utils.save_model(one_step_model, export_dir)
+    print(message)
+    return history
+
+def clean_dataset(configs: dict) -> Path:
+    # Load configuration parameters
+    configs = SimpleNamespace(**configs)
+    # Clean the data and save to a local file
+    project_root = misc.get_project_root()
+    data_dir = project_root.joinpath(configs.dataset_dir_name)
+    cleaner = Cleaner(data_export_dir=data_dir)
+    cleaned_file_path = cleaner(hf_dataset=configs.huggingface_dataset, dataset_frac=configs.dataset_frac)
+    return cleaned_file_path
 
 if __name__ == "__main__":
     config_dict = misc.load_config("config.yaml")
-    train_char_lm(config_dict)
+    cleaned_file_path = clean_dataset(config_dict)
+    history = train_char_lm(config_dict, cleaned_file_path)
+
